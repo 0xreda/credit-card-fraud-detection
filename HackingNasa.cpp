0aff333c785ebd462bb2a8fe35954dd9a6cdbe93
Reda Hilal <zzawarudo04@gmail.com>
2024-12-27 08:24:29 +0400

    Created project structure.

---
+++ HackingNasa.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	cout << "Hacking..." << endl;
+
+
+	return 0;
+}
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # credit-card-fraud-detection
 Credit Card Fraud Detection Repository for SQA Assignment
+...
