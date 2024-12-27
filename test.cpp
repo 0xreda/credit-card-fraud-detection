1bc941c12d20d542352db828097c107f185d4fa2
Reda Hilal <mohammed@gmail.com>
2024-12-27 09:22:56 +0400

    added a c++ test file.

---
+++ test.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	cout << "Hacking..." << endl;
+	cout << "Hacking..." << endl;
+	cout << "Hacking..." << endl;
+	cout << "Hacking..." << endl;
+	cout << "Hacking..." << endl;
+	cout << "Hacking..." << endl;
+	cout << "Hacking..." << endl;
+	cout << "Hacking..." << endl;
+	cout << "Hacking..." << endl;
+	cout << "Hacking..." << endl;
+	cout << "Hacking..." << endl;
+	cout << "Hacking..." << endl;
+	cout << "Hacking..." << endl;
+	cout << "Hacking..." << endl;
+	cout << "Hacking..." << endl;
+	cout << "Hacking..." << endl;
+	cout << "Hacking..." << endl;
+	cout << "Hacking..." << endl;
+	cout << "Hacking..." << endl;
+	cout << "Hacking..." << endl;
+	cout << "Hacking..." << endl;
+	cout << "Hacking..." << endl;
+	cout << "Hacking..." << endl;
+	cout << "Hacking..." << endl;
+	cout << "Hacking..." << endl;
+	cout << "Hacking..." << endl;
+
+
+	return 0;
+}
